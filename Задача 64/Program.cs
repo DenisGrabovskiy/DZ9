1128b75fad38ad603f0a75bd5e9f3ef65219dfe0
Грабовский Денис <g.d.a@bk.ru>
2023-11-18 23:07:23 +0300

    Задача 64

---
+++ Задача 64/Program.cs
@@ -0,0 +1,26 @@
+﻿ 
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    int result=Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void PrintNumbers(int n)
+{
+    if (n<=1)
+    {
+        Console.Write($"{n}\t");
+        return;
+    }
+    Console.Write($"{n}\t");
+    PrintNumbers(n-1);
+}
+
+int n=ReadInt("Введите натуральное число: ");
+while (n<1)
+{
+    Console.Write("Число не натуральное. ");
+    n=ReadInt("Введите натуральное число: ");
+}
+PrintNumbers(n);
